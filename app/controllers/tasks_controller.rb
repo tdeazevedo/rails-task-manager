e7ea34cc41021f317454d168930b6eb334f456ff
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-03-12 17:13:50 +0100

    add delete action and refacto

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -17,24 +18,19 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   # Update
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
 
     redirect_to tasks_path
   end
 
-  # Delete
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     redirect_to tasks_path
@@ -42,6 +38,10 @@ class TasksController < ApplicationController
 end
 
 private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
