1d45f445e1d46dfbf5c2a8d52a9646b554b77202
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-03-12 12:29:11 +0100

    RTM add defaut to completed column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_105822) do
+ActiveRecord::Schema.define(version: 2019_03_12_111745) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
